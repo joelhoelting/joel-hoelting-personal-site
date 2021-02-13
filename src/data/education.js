886b32c2abb66f1cf45bfe5ec43dc038d4ef1d5c
Joel Hoelting <joel@joelhoelting.com>
2021-02-12 21:42:34 -0500

    Updates education data

--- src/data/education.js
+++ src/data/education.js
@@ -4,15 +4,15 @@ const educationArray = [
     subHeading: 'New York, NY',
     endDate: '2018',
     startDate: '2017',
-    listDescription: '1000+ Hours Software Development Bootcamp with 6% Acceptance Rate.'
+    listDescription: '1000+ Hours Software Development Bootcamp with 6% Acceptance Rate.',
   },
   {
     heading: 'Hunter College',
     subHeading: 'New York, NY',
-    endDate: '2015',
-    startDate: '2011',
-    listDescription: 'Bachelor of Science, Economics'
-  }
+    endDate: '2016',
+    startDate: '2012',
+    listDescription: 'Bachelor of Science, Economics',
+  },
 ];
 
 export default educationArray;
